cf72ae1ab0b639ebf9cd0768b37e08af7594416c
turpp <white.tripp@icloud.com>
2021-01-14 16:16:22 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let record =  array.find(function(object){
+    return object.result === 'W'
+})
+  if (record){
+    return record.year
+  }
+}
+
+
+
+
